c3759ecfb34290f607b661b90c39506f26d99df4
Ayabusa <lebgpub@gmail.com>
2026-05-06 13:45:08 +0200

    fix again ?

--- install-void.sh
+++ install-void.sh
@@ -54,10 +54,10 @@ mkdir -p "/home/ayabusa/PortableApps"
 chown -R ayabusa "/home/ayabusa/Projects" "/home/ayabusa/PortableApps"
 
 echo "Downloading some cool appimages"
-wget "https://vencord.dev/download/vesktop/amd64/appimage" -P "/home/ayabusa/PortableApps/vesktop.AppImage"
-wget "https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/latest/download/DesktopEditors-x86_64.AppImage" -P "/home/ayabusa/PortableApps/only-office.AppImage"
-wget "https://github.com/Alex313031/thorium/releases/latest/download/Thorium_Browser_138.0.7204.303_AVX2.AppImage" -P "/home/ayabusa/PortableApps/thorium.AppImage" 
-wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip" -P "/home/ayabusa/Downloads/JetBrainsMono.zip"
+wget "https://vencord.dev/download/vesktop/amd64/appimage" -O "/home/ayabusa/PortableApps/vesktop.AppImage"
+wget "https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/latest/download/DesktopEditors-x86_64.AppImage" -O "/home/ayabusa/PortableApps/only-office.AppImage"
+wget "https://github.com/Alex313031/thorium/releases/latest/download/Thorium_Browser_138.0.7204.303_AVX2.AppImage" -O "/home/ayabusa/PortableApps/thorium.AppImage" 
+wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip" -O "/home/ayabusa/Downloads/JetBrainsMono.zip"
 chown ayabusa /home/ayabusa/PortableApps/*
 echo "You'll need to install them yourself they are in /home/ayabusa/PortableApps"
 
@@ -68,13 +68,13 @@ rm -f "/home/ayabusa/Downloads/JetBrainsMono.zip"
 fc-cache -f -v
 
 echo "Installing Bibata cursors"
-wget "https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Classic.tar.xz" -P "/home/ayabusa/Downloads/cursor.tar.xz"
+wget "https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Classic.tar.xz" -O "/home/ayabusa/Downloads/cursor.tar.xz"
 mkdir -p "/home/ayabusa/.icons"
 tar -xvf "/home/ayabusa/Downloads/cursor.tar.xz" -C "/home/ayabusa/.icons/"
 rm "/home/ayabusa/Downloads/cursor.tar.xz"
 
 echo "Downloading GTK theme, you'll need to install it yourself"
-wget "https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/archive/refs/heads/main.zip" -P "/home/ayabusa/PortableApps/catpuccin_gtk.zip"
+wget "https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme/archive/refs/heads/main.zip" -O "/home/ayabusa/PortableApps/catpuccin_gtk.zip"
 
 echo "Copying the backgrounds into Pictures"
 cp config/bg/* "/home/ayabusa/Pictures"
